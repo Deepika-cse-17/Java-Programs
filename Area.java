42b43ce21bc4e43b26999462f75ec044da4f7d99
Deepika <deepikasabarinathan@gmail.com>
2025-07-30 17:19:29 +0530

    Add files via upload

--- Area.java
+++ Area.java
@@ -1,4 +1,12 @@
-
+// Area of the rectangle
 public class Area {
+		   public static void main(String arg[])
+		   {
+			   //declaration of variables
+			   int a=5,b=5;
+			   int c=a*b;
+			   System.out.println("The area of the rectangle is "+c);
+		   }
+	
 
 }
