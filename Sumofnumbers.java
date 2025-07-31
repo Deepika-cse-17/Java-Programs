df0185553d22d8343b1e9e7086007cc767f0b0fc
Deepika <deepikasabarinathan@gmail.com>
2025-07-31 21:08:39 +0530

    Add files via upload

---
+++ Sumofnumbers.java
@@ -0,0 +1,13 @@
+//sum of numbers
+public class Sumofnumbers {
+	public static void main (String arg[])
+	{
+		int sum=0;
+		for(int i=0; i<=100; i++)
+		{
+			sum+=i;
+		}
+		System.out.println("The sum of numbers from 1 to 100 : "+sum);
+	}
+
+}
